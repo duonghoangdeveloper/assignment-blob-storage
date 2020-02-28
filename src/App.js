fa7aa66cdf9fd85b7db1509688fc0634f2cb8f9a
duonghoangdeveloper <duonghoangdeveloper@gmail.com>
2020-02-29 06:50:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [blobs, setBlobs] = useState([]);
+
+  function onChangeFile(e) {
+    const f = Array.from(e.target.files)[0];
+    setFile(f);
+  }
+
+  function getBlobs() {
+    if (window.blobService) {
+      window.blobService.listBlobsSegmented('images', null, function(
+        error,
+        results
+      ) {
+        if (error) {
+          // List blobs error
+        } else {
+          setBlobs(results.entries);
+        }
+      });
+    }
+  }
+
+  useEffect(() => {
+    getBlobs();
+  }, []);
+
+  function handleUpload() {
+    if (!file) {
+      alert('File is empty!');
+      return;
+    }
+
+    if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
+      alert('File is not an image!');
+      return;
+    }
+
+    if (file.size >= 1024 * 1024) {
+      alert('File size must not smaller than 1MB!');
+      return;
+    }
+
+    const customBlockSize =
+      file.size > 1024 * 1024 * 32 ? 1024 * 1024 * 4 : 1024 * 512;
+    window.blobService.singleBlobPutThresholdInBytes = customBlockSize;
+
+    window.blobService.createBlockBlobFromBrowserFile(
+      'images',
+      file.name,
+      file,
+      { blockSize: customBlockSize },
+      function(error, result, response) {
+        if (error) {
+          // Upload blob failed
+        } else {
+          getBlobs();
+        }
+      }
+    );
+  }
+
+  function handleDelete(blobName) {
+    return function() {
+      window.blobService.deleteBlobIfExists('images', blobName, function(
+        error,
+        result
+      ) {
+        if (error) {
+          // Delete blob failed
+        } else {
+          getBlobs();
+        }
+      });
+      console.log(blobName);
+    };
+  }
+
+  console.log(blobs);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+      }}
+    >
+      <h1>Upload Image</h1>
+      <input type="file" onChange={onChangeFile} />
+      <br />
+      <button type="button" onClick={handleUpload}>
+        Upload
+      </button>
+      <br />
+      {blobs.map((image, i) => (
+        <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+          <img
+            key={i}
+            alt={i}
+            style={{ width: 300, display: 'block' }}
+            src={`${window.blobUri}/images/${image.name}`}
+          />
+          <button type="button" onClick={handleDelete(image.name)}>
+            Delete
+          </button>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
