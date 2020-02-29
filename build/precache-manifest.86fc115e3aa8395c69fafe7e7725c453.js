bb0928601c26d38549871cceb2d6dacd524ab10f
duonghoangdeveloper <duonghoangdeveloper@gmail.com>
2020-02-29 20:32:38 +0700

    deployment

---
+++ build/precache-manifest.86fc115e3aa8395c69fafe7e7725c453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d485ea18b69b2f7ba887038f427427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3925885339c805faf8b9",
+    "url": "/static/js/2.c1cca821.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c1cca821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af42e10922f2682e3876",
+    "url": "/static/js/main.c3c9bf11.chunk.js"
+  },
+  {
+    "revision": "e4bee06d525e7c564ac6",
+    "url": "/static/js/runtime-main.13db2964.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,1 @@
+var express = require('express'); var server = express(); var options = { index: 'build/index.html' }; server.use('/', express.static('/home/site/wwwroot', options)); server.listen(process.env.PORT);
